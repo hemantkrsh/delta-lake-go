beea2bd66010be00da73d6b7951255c66925c342
hemant <hemantkrsingh17@gmail.com>
2026-01-17 16:49:35 +0530

    [refact]: handled unknown operator in expression

--- expression.go
+++ expression.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 )
 
-var errUnknownOperator = errors.New("Unknown Operator")
+var errUnknownOperator = errors.New("unknown operator")
 
 type expression interface{}
 
@@ -23,21 +23,21 @@ type simpleExpression struct {
 	right    expression // would be a primitive type int,float,string etc
 }
 
-func (r row) filter(expr simpleExpression) bool {
+func (r row) filter(expr simpleExpression) (bool, error) {
 	return eval(string(expr.operator), r[expr.left], expr.right)
 }
 
 // todo: change signature to eval(row,expr) can be evolved to complexExpr as well which calls each []simpleExpr in loop
-func eval(cond string, leftValue any, rightValue any) bool {
+func eval(cond string, leftValue any, rightValue any) (bool, error) {
 	switch cond {
 	case "==":
 		switch reflect.TypeOf(leftValue) {
 		case reflect.TypeOf(int(0)):
-			return leftValue.(int) == rightValue.(int)
+			return leftValue.(int) == rightValue.(int), nil
 		case reflect.TypeOf(string("")):
-			return strings.Compare(leftValue.(string), rightValue.(string)) == 0
+			return strings.Compare(leftValue.(string), rightValue.(string)) == 0, nil
 		default:
-			return false
+			return false, nil
 		}
 
 	case ">":
@@ -45,7 +45,7 @@ func eval(cond string, leftValue any, rightValue any) bool {
 		switch reflect.TypeOf(leftValue) {
 		case reflect.TypeOf(int(0)):
 			log.Printf("expr result in eval: %v", leftValue.(int) > rightValue.(int))
-			return leftValue.(int) > rightValue.(int)
+			return leftValue.(int) > rightValue.(int), nil
 		case reflect.TypeOf(float64(0.0)):
 			rt, err := strconv.ParseFloat(rightValue.(string), 64)
 			if err != nil {
@@ -53,48 +53,48 @@ func eval(cond string, leftValue any, rightValue any) bool {
 			}
 			result := leftValue.(float64) > rt
 			log.Printf("expr result in eval: %v", result)
-			return result
+			return result, nil
 		case reflect.TypeOf(string("")):
-			return strings.Compare(leftValue.(string), rightValue.(string)) == 1
+			return strings.Compare(leftValue.(string), rightValue.(string)) == 1, nil
 		default:
-			return false
+			return false, nil
 		}
 
 	case "<":
 		switch reflect.TypeOf(leftValue) {
 		case reflect.TypeOf(int(0)):
-			return leftValue.(int) < rightValue.(int)
+			return leftValue.(int) < rightValue.(int), nil
 		case reflect.TypeOf(string("")):
-			return strings.Compare(leftValue.(string), rightValue.(string)) == -1
+			return strings.Compare(leftValue.(string), rightValue.(string)) == -1, nil
 		default:
-			return false
+			return false, nil
 		}
 	case "<>":
 		switch reflect.TypeOf(leftValue) {
 		case reflect.TypeOf(int(0)):
-			return !(leftValue.(int) == rightValue.(int))
+			return !(leftValue.(int) == rightValue.(int)), nil
 		case reflect.TypeOf(string("")):
-			return !(strings.Compare(leftValue.(string), rightValue.(string)) == 0)
+			return !(strings.Compare(leftValue.(string), rightValue.(string)) == 0), nil
 		default:
-			return false
+			return false, nil
 		}
 	default:
-		return false
+		return false, errUnknownOperator
 
 	}
 }
 
 // TODO: there should be better ways to do this
-func getTypeFrmStrVal(stringValue string) any {
-	for {
-		if val, err := strconv.Atoi(stringValue); err != nil {
-			return val
-		}
-		if val, err := strconv.ParseBool(stringValue); err != nil {
-			return val
-		}
-		if val, err := strconv.ParseFloat(stringValue, 64); err != nil {
-			return val
-		}
-	}
-}
+// func getTypeFrmStrVal(stringValue string) any {
+// 	for {
+// 		if val, err := strconv.Atoi(stringValue); err != nil {
+// 			return val
+// 		}
+// 		if val, err := strconv.ParseBool(stringValue); err != nil {
+// 			return val
+// 		}
+// 		if val, err := strconv.ParseFloat(stringValue, 64); err != nil {
+// 			return val
+// 		}
+// 	}
+// }
